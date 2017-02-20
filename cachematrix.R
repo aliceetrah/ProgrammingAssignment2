9746019ca73e58601e2ac7ea6f8e00757dddd2a2
aliceetrah <harte.ali@gmail.com>
2017-02-20 09:03:02 +0000

    Caching Assignment
    Caching the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This code contains two R functions that cache getting the inverse of a matrix
+## The calculation is done once, and stored in the cache so that it is not computed more than once
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  get <- function() x
+  set_inverse <- function(inv) inverse <<- inv
+  get_inverse <- function() inverse
+  list(set = set, get = get,
+       set_inverse = set_inverse,
 }
 
 
